e23de11e7d37e713c53d6c29db617425d17d8150
mahaancoder <sachan.satyam@gmail.com>
2026-03-27 01:10:39 +0530

    removed backtest

--- src/main/java/com/satyam/trading/backtest/BacktestRunner.java
+++ src/main/java/com/satyam/trading/backtest/BacktestRunner.java
@@ -14,6 +14,6 @@ public class BacktestRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        backtestEngine.runBacktest( SampleData.getData(), "RELIANCE");
+//        backtestEngine.runBacktest( SampleData.getData(), "RELIANCE");
     }
 }
